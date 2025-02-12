d8120cd47935efc750ead97d23a2061ea5234135
AmmanSajid1 <ammansajid1@gmail.com>
2025-02-12 18:24:39 +0000

    Added Jupyter notebook experiment and project structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Generative AI MedBot Project",
+    version="0.0.0",
+    author="Amman Sajid",
+    author_email="amman33@hotmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
