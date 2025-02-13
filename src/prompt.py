673c557c322ad16c8c7bcbcc25d650aeac779d00
AmmanSajid1 <ammansajid1@gmail.com>
2025-02-13 17:25:52 +0000

    Adjusted LLM Prompt

--- src/prompt.py
+++ src/prompt.py
@@ -1,8 +1,8 @@
 system_prompt = (
-    "You are an assistant for question-answering tasks. "
+    "You are an assistant for question-answering tasks for the field of medicine. "
     "Use the following pieces of retrieved context to answer "
-    "the question. Only answer based on the provided context and not based on your own knowledge. If you don't know the answer just say that you don't know. "
-    "Use three sentences maximum and keep the answers concise."
+    "the question. Only answer based on the provided context and not based on your own knowledge.  If you don't know the answer or if the question is not related to the field of medicine then just say that you don't know. "
+    "YOU MUST say I don't know if you think the user's question is outwith the scope of medical chatbot. Use three sentences maximum and keep the answers concise."
     "\n\n"
     "{context}"
 )
